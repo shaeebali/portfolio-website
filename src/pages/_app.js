93e6c47e46649cc9a0634eb7975b98b94e8ed9d1
shaeeb <shaeebali@gmail.com>
2023-12-02 19:44:14 +0000

    complete footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { Main } from 'next/document';
 import {Montserrat} from 'next/font/google'
 import Head from 'next/head';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }) {
   <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
     <NavBar />
     <Component {...pageProps} />
+    <Footer />
   </main>
   </>
   )
