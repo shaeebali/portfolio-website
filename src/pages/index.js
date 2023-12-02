ae8dff6947bf6159c528a8adf12c8a04e31796f9
shaeeb <shaeebali@gmail.com>
2023-12-02 12:55:16 +0000

    complete resume and contact buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link';
 import profilePic from '../../public/images/profile/developer-pic-1.png';
 import AnimatedText from '../components/AnimatedText';
+import { LinkArrow } from '../components/Icons';
 
 export default function Home() {
   return (
@@ -24,8 +25,15 @@ export default function Home() {
               <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
-              <div>
-                <Link href='dummy.pdf'>Resume</Link>
+              <div className='flex items-center self-start mt-2'>
+                <Link href='dummy.pdf' target={"_blank"}
+                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'
+                download={true}
+                >Resume <LinkArrow className='w-6 ml-1'/>
+                </Link>
+                <Link href='mailto:abcd@gmail.com'target={"_blank"}
+                className='ml-4 text-lg font-medium capitalize text-dark underline'
+                >Contact</Link>
               </div>
             </div>
           </div>
