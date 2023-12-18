543d6f73e72fd18fb90e42c377689600540e5310
shaeeb <shaeebali@gmail.com>
2023-12-18 09:42:43 +0000

    complete education/experience components

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ import profilePic from '../../public/images/profile/developer-pic-2.jpg'
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 import Skills from '../components/Skills';
 import Experience from '../components/Experience';
+import Education from '../components/Education';
 
 const AnimatedNumbers = ({value}) => {
   const ref = useRef(null);
@@ -78,6 +79,7 @@ const about = () => {
       </div>
       <Skills />
       <Experience />
+      <Education />
       </Layout>
     </main>
    </> 
