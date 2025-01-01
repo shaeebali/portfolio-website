3656ec477180df121006984ccea130c17a3433e1
shaeeb <shaeebali@gmail.com>
2025-01-01 12:10:54 +0000

    dark mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,11 @@
+
 import Link from 'next/link';
 import React from 'react';
 import { useRouter } from 'next/router';
 import Logo from './Logo';
-import { TwitterIcon, GithubIcon, LinkedInIcon, PinterestIcon, DribbleIcon } from './Icons';
+import { TwitterIcon, GithubIcon, LinkedInIcon, PinterestIcon, DribbleIcon, SunIcon, MoonIcon } from './Icons';
 import { motion } from 'framer-motion';
+import useThemeSwitcher from './hooks/useThemeSwitcher';
 
 const CustomLink = ({href, title, className=''}) => {
   const router = useRouter();
@@ -11,7 +13,7 @@ const CustomLink = ({href, title, className=''}) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
 
-      <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>
+      <span className={`dark:bg-light h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>
 
       </span>
     </Link>
@@ -19,9 +21,12 @@ const CustomLink = ({href, title, className=''}) => {
 } 
 
 const NavBar = () => {
+  
+  const [mode, setMode] = useThemeSwitcher();
+  
   return (  
     <header
-    className='w-full px-32 py-8 font-medium flex items-center justify-between'
+    className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'
     >
       <nav>
         <CustomLink href='/' title='Home' className='mr-4'/>
@@ -66,6 +71,17 @@ const NavBar = () => {
         >
           <DribbleIcon />
         </motion.a>
+      
+        <button 
+          onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+          className={ `ml-4 flex items-center justify-center rounded-full p-1
+          ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}
+          `}
+        >
+          {
+            mode === 'dark' ? <SunIcon className='fill-dark' /> : <MoonIcon className='fill-dark' />
+          }
+        </button>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />
