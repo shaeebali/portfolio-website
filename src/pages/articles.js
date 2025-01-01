5f98e3c7807908b42478ef783177a7d46ac2cefc
shaeeb <shaeebali@gmail.com>
2025-01-01 10:28:31 +0000

    add articles section

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import AnimatedText from '../components/AnimatedText';
@@ -6,9 +6,58 @@ import Link from 'next/link';
 import Image from 'next/image';
 import article1 from '../../public/images/articles/pagination component in reactjs.jpg';
 import article2 from '../../public/images/articles/create loading screen in react js.jpg';
-import { motion } from 'framer-motion';
+import article3 from '../../public/images/articles/create modal component in react using react portals.png';
+import article4 from '../../public/images/articles/form validation in reactjs using custom react hook.png';
+import article5 from '../../public/images/articles/smooth scrolling in reactjs.png';
+import { motion, useMotionValue } from 'framer-motion';
 
 const FramerImage = motion(Image);
+
+const MovingImg = ({title, img, link}) => {
+  
+  const x =  useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = 'inline-block';
+    x.set(event.pageX);
+    y.set(-10);
+  }
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = 'none';
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+  <Link href={link} target='_blank'
+    onMouseMove={handleMouse}
+    onMouseLeave={handleMouseLeave}
+  >
+    <h2 className='capitalize text-xl font-semibold hover:underline'>{title}</h2>
+    <FramerImage 
+      style={{x: x, y: y}}
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1, transition: {duration: 0.5}}}
+      ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg' 
+    />
+  </Link>
+  );
+}  
+
+const Article = ({img, title, date, link}) => {
+  return (
+    <motion.li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'
+    initial={{y: 200}}
+    whileInView={{y: 0, transition: {duration: 0.5, ease: 'easeInOut'}}}
+    viewport={{once: true}}
+    >
+      <MovingImg title={title} img={img} link={link} />
+      <span className='text-primary font-semibold pl-4'>{date}</span>
+    </motion.li>
+  );
+}
 
 const FeaturedArticle = ({img, title, time, summary, link}) => {
   return (
@@ -36,7 +85,7 @@ const Articles = () => {
   return (
     <>
       <Head>
-        <title>Shaeeb Ali | About Page</title>
+        <title>Shaeeb Ali | Articles Page</title>
         <meta name='description' content='any description' />
       </Head>
       <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
@@ -59,6 +108,26 @@ const Articles = () => {
             />
           </ul>
           <h2 className='font-bold text-4xl w-full text-center my-16 mt-32'>All Articles</h2>
+          <ul>
+            <Article 
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+              date="March 22, 2024"
+              link="/"
+              img={article3}
+            />
+            <Article 
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+              date="March 22, 2024"
+              link="/"
+              img={article4}
+            />
+            <Article 
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+              date="March 22, 2024"
+              link="/"
+              img={article5}
+            />
+          </ul>
         </Layout>
       </main>
     </>
