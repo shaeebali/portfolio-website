88c1ac2a118bfd49197a68ef49f60453829fccb2
shaeeb <shaeebali@gmail.com>
2025-01-02 12:03:45 +0000

    add responsiveness

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -48,20 +48,20 @@ const MovingImg = ({title, img, link}) => {
 
 const Article = ({img, title, date, link}) => {
   return (
-    <motion.li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4'
+    <motion.li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light'
     initial={{y: 200}}
     whileInView={{y: 0, transition: {duration: 0.5, ease: 'easeInOut'}}}
     viewport={{once: true}}
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className='text-primary font-semibold pl-4'>{date}</span>
+      <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
     </motion.li>
   );
 }
 
 const FeaturedArticle = ({img, title, time, summary, link}) => {
   return (
-    <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl'>
+    <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light'>
       <Link 
         href={link} 
         target='_blank' 
@@ -70,13 +70,17 @@ const FeaturedArticle = ({img, title, time, summary, link}) => {
           <FramerImage src={img} alt={title} className='w-full h-auto' 
             whileHover={{scale: 1.05}}
             transition={{duration: 0.2}}
+            priority
+            sizes='(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            50vw'
           />
       </Link>
       <Link href={link} target='_blank'>
         <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>{title}</h2>
       </Link>
       <p className='text-sm mb-2'>{summary}</p>
-      <span className='text-primary font-semibold'>{time}</span>  
+      <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>  
     </li>
   );
 };
@@ -88,7 +92,7 @@ const Articles = () => {
         <title>Shaeeb Ali | Articles Page</title>
         <meta name='description' content='any description' />
       </Head>
-      <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
+      <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText text='Words Can Change The World!' className='mb-16'/>
           <ul className='grid grid-cols-2 gap-16'>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,7 +12,7 @@ const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
   return(
-    <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12'>
+    <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:border-light dark:bg-dark dark:text-light'>
       
       
       <Link href={link} target='_blank'
@@ -21,19 +21,23 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
         <FramerImage src={img} alt={title} className='w-full h-auto'
           whileHover={{scale: 1.05}}
           transition={{duration: 0.2}}
+          priority
+          sizes='(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            50vw'
         />
       </Link>
     
       <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-        <span className='text-primary font-medium text-xl'>{type}</span>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
           <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
         </Link>
-        <p className='my-2 font-medium text-dark'>{summary}</p>
+        <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
         <div className='mt-2 flex items-center'>
         <Link href={github} target='_blank' className='w-10'> <GithubIcon /> </Link>
         <Link href={link} target='_blank'
-        className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold'
+        className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark'
         > Visit Project </Link>
         </div>
       </div>
@@ -44,7 +48,7 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
 const Project = ({title, type, img, link, github}) => {
   return (
-    <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative'>
+    <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:border-light dark:bg-dark dark:text-light'>
       <Link href={link} target='_blank'
       className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
@@ -55,7 +59,7 @@ const Project = ({title, type, img, link, github}) => {
       </Link>
     
       <div className='w-full flex flex-col items-start justify-between mt-4'>
-        <span className='text-primary font-medium text-xl'>{type}</span>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
           <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
         </Link>
