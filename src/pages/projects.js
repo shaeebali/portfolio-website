69a94523418343acdd52784583a4901a8d5bc2a9
shaeeb <shaeebali@gmail.com>
2025-01-06 12:01:53 +0000

    add responsiveness to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,11 +12,12 @@ const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
   return(
-    <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:border-light dark:bg-dark dark:text-light'>
+    <article className='w-full flex items-center justify-between relative rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:border-light dark:bg-dark dark:text-light
+    lg:flex-col lg:p-8 xs:rounded-br-3xl xs:rounded-2xl xs:p-4'>
       
-      
+      {/* <div className='absolute top-0 right-3 -z-10 w-[101%] h-[103%] bg-dark rounded-[2.5rem] dark:bg-light' /> */}
       <Link href={link} target='_blank'
-      className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+      className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'
       >
         <FramerImage src={img} alt={title} className='w-full h-auto'
           whileHover={{scale: 1.05}}
@@ -28,16 +29,16 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
         />
       </Link>
     
-      <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+      <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+          <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm'>{title}</h2>
         </Link>
-        <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+        <p className='my-2 font-medium text-dark dark:text-light sm:text-sm'>{summary}</p>
         <div className='mt-2 flex items-center'>
         <Link href={github} target='_blank' className='w-10'> <GithubIcon /> </Link>
         <Link href={link} target='_blank'
-        className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark'
+        className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base'
         > Visit Project </Link>
         </div>
       </div>
@@ -48,7 +49,8 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
 const Project = ({title, type, img, link, github}) => {
   return (
-    <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:border-light dark:bg-dark dark:text-light'>
+    <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:border-light dark:bg-dark dark:text-light
+    xs:p-4'>
       <Link href={link} target='_blank'
       className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
@@ -59,17 +61,17 @@ const Project = ({title, type, img, link, github}) => {
       </Link>
     
       <div className='w-full flex flex-col items-start justify-between mt-4'>
-        <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-3xl font-bold'>{title}</h2>
+          <h2 className='my-2 w-full text-left text-3xl font-bold lg:text-2xl '>{title}</h2>
         </Link>
         
         <div className='w-full mt-2 flex items-center justify-between'>
         <Link href={link} target='_blank'
-        className='text-lg font-semibold underline'
+        className='text-lg font-semibold underline md:text-base'
         > Visit 
         </Link>
-        <Link href={github} target='_blank' className='w-8'> <GithubIcon /> 
+        <Link href={github} target='_blank' className='w-8 md:width-6'> <GithubIcon /> 
         </Link>
         
         </div>
@@ -88,13 +90,13 @@ const projects = () => {
       <main className='w-full mb-16 flex flex-col items-center justify-center'>
         <Layout className='pt-16'>
           <AnimatedText text='Imagination Trumps Knowledge!' 
-          className='mb-16'
+          className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'
           />
 
-          <div className='grid grid-cols-12 gap-24'>
+          <div className='grid grid-cols-12 gap-y-24 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
             <div className='col-span-12'>
               <FeaturedProject 
-              title='Crypto Screener Application'
+              title='1. Crypto Screener Application'
               img={project1}
               summary=' feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
               It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -106,7 +108,7 @@ const projects = () => {
             </div>
             <div className='col-span-6'>
             <Project 
-              title='Crypto Screener Application'
+              title='2. Crypto Screener Application'
               img={project1}
               summary=' feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
               It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -118,7 +120,7 @@ const projects = () => {
             </div>
             <div className='col-span-6'>
             <Project 
-              title='Crypto Screener Application'
+              title='3. Crypto Screener Application'
               img={project1}
               summary=' feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
               It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
@@ -129,10 +131,10 @@ const projects = () => {
               />
           </div>
             </div>
-          <div className='grid grid-cols-12 gap-24 pt-16'>
+          <div className='grid grid-cols-12 gap-y-24 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
             <div className='col-span-12'>
             <FeaturedProject 
-              title='Crypto Screener Application'
+              title='4. Crypto Screener Application'
               img={project1}
               summary=' feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
               It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
